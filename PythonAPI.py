5b47340b71f772cf8c04b94062026ab2ca0ae6f4
Rashida Makati <33939213+rashidamakati@users.noreply.github.com>
2019-04-13 19:05:04 -0700

    Add files via upload

---
+++ PythonAPI.py
@@ -0,0 +1,46 @@
+# Write a python program that takes input from a file and outputs to another file. The input file consists of n lines. Each line will be comma separated values. The first work will be an IP address followed by various key attributes.
+# The output will be a file which has n lines in a comma separated values. The first word will be the ip address followed by values of the key attributes from the input file for that ip address.
+# Use this api to get information for that IP.
+# https://ipapi.co/api/#introduction
+#
+# Input file and output file name will be give via command line parameters. You must write your own function to validate the IP address. Your code must use object oriented classes and objects as much as possible.
+#
+# If IP is not valid, output line must say INVALID IP
+# If any key attribute is not valid for any input IP, output must say INVALID KEY for that key value.
+# If key attributes values are comma separated, you must replace comma with :
+#
+# Eg.
+# $ ip_info.py input.txt output.txt
+#
+# input.txt-
+# 8.8.8.8,city,region,country,postal
+# 123.10.100.50,asn,org,in_eu,region_code
+# 600.40.12.100,timezone,utc_offset,asn,org
+# 5.225.26.154,postal,languages,public
+#
+# output.txt-
+# 8.8.8.8,Mountain View,California,United States,94035
+# 123.10.100.50,AS4837,CHINA UNICOM China169 Backbone,false,null
+# INVALID IP
+# 5.225.26.154,15572,es-ES:ca:gl:eu:oc,INVALID KEY
+
+from requests import get
+import json
+collective_data = []
+with open("input.txt") as file:
+    for lines in file:
+        line_data = lines
+        split_data = line_data.split(',')
+        ipaddress = split_data[0]
+        collect_data = [ipaddress]
+        for j in range(1,len(split_data)-1,1):
+            valueip = get('https://ipapi.co/'+str(ipaddress)+'/'+str(split_data[j])+'/').text
+            collect_data.append(valueip)
+        collective_data.append(collect_data)
+
+file.close()
+
+with open('output.txt', 'w') as f:
+    for item in collective_data:
+        item = ','.join(item)
+        f.write("%s\n" % item)
